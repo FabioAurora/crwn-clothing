495ce3bd9e231796fb4397bf769b7d9236913aec
Fabio Aurora <fabioauroradev@gmail.com>
2023-07-13 20:10:07 +0100

    updating directory page with directory component and category item component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./categories.styles.scss";
+import Directory from "./Components/Directory/Directory";
+
+const App = () => {
+
+  return (
+    <Directory />
+  );
+};
+
+export default App;

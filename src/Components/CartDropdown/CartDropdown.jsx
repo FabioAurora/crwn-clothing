c01f467871ac663a8d34747a3279461e74e8d872
Fabio Aurora <fabioauroradev@gmail.com>
2023-07-31 16:57:04 +0100

    feat: helper function to add cart item to the cart, array method (find, map).

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './CartDropdown.styles.scss';
 import Button from '../Button/Button';
 
+import CartItem from '../CartItem/CartItem';
+
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext)
     return (
         <div className='cart-dropdown-container'>
-            <div className='cart-items' />
+            <div className='cart-items'>
+                {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
+            </div>
             <Button>Go To Checkout</Button>
         </div>
     )
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,15 +1,37 @@
 import { createContext, useState } from "react";
+import CartItem from "../Components/CartItem/CartItem";
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(CartItem => CartItem.id === productToAdd.id)
+  // if found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map(CartItem => CartItem.id === productToAdd.id
+      ? {...CartItem, quantity: CartItem.quantity + 1}
+      : CartItem
+      );
+  };
+  // return new array with modified cartItems/ new cart item
+  //[{...productToAdd, quantity: 1}]
+  return [...cartItems, {...productToAdd, quantity: 1}];
+};
 
 export const CartContext = createContext({
-    isCartOpen: false,
-    setIsCartOpen: () => {},
-})
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+});
 
-export const CartProvider = ({children}) => {
-    const [isCartOpen,  setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen};
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
-    return (
-        <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    )
-}
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
